7e9b8750aab2146923de42bcc31cd6dc81f415dc
Zakaria <zgouiss15@gmail.com>
2026-08-16 16:19:59 -0400

    Misc. responsive changes w radio buttons

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ import SemesterModal from "./components/header/SemesterModal.jsx";
 
 function App() {
   const [viewingWeek, setViewingWeek] = useState(null);
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(true);
   const [assignmentModalOpen, setAssignmentModalOpen] = useState(false);
   const [classModalOpen, setClassModalOpen] = useState(false);
   const [editAssignmentModalOpen, setEditAssignmentModalOpen] = useState(false);
